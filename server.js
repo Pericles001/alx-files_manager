651619d41a4790a2366be4892a774e1ad17a341f
Pericles001 <periclesadjovi@gmail.com>
2022-08-23 20:41:19 +0100

    Update server file with mainRouter constant

--- server.js
+++ server.js
@@ -1,14 +1,12 @@
 import express from 'express';
 import { env } from 'process';
-import router from './routes/index';
+
+const mainRoute = require('./routes/index');
 
 const app = express();
 const port = env.PORT || 5000;
-const routes = require('./routes/index');
-
 app.use(express.json());
-app.use(router);
+app.use(mainRoute);
 app.listen(port, '127.0.0.1', () => console.log(`Server started on port ${port}`));
-
 
 export default app;
