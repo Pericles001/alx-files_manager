5e6c14911a488bda9acea9be8ba456c4710f7f94
Pericles001 <periclesadjovi@gmail.com>
2022-08-23 20:41:42 +0100

    Create methods to retrieve datas

---
+++ routes/index.js
@@ -0,0 +1,10 @@
+import { Router } from 'express';
+
+import AppController from '../controllers/AppController';
+
+const router = Router();
+
+router.get('/status', AppController.getStatus);
+router.get('/stats', AppController.getStats);
+
+module.exports = router;
