9fd6cf23cc821d0aee25d79a0ae190cd72b1f339
Pericles001 <periclesadjovi@gmail.com>
2022-08-23 20:54:02 +0100

    Update file content

--- server.js
+++ server.js
@@ -7,6 +7,6 @@ const app = express();
 const port = env.PORT || 5000;
 app.use(express.json());
 app.use(mainRoute);
-app.listen(port, '127.0.0.1', () => console.log(`Server started on port ${port}`));
+app.listen(port, '127.0.0.1');
 
 export default app;
