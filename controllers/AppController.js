cc644ef06dcd6dc3baa27ab2c8d570b73d0930f4
Pericles001 <periclesadjovi@gmail.com>
2022-08-24 15:30:29 +0100

    Update file content

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -2,18 +2,20 @@ import redisClient from '../utils/redis';
 import dbClient from '../utils/db';
 
 class AppController {
-  static async getStats(request, response) {
-    const users = await dbClient.nbUsers();
-    const files = await dbClient.nbFiles();
-    response.set('Content-Type', 'application/json');
-    response.status(200).send(JSON.stringify({ users, files })).end();
-  }
-
-  static async getStatus(request, response) {
+  static getStatus(request, response) {
     const redisStatus = redisClient.isAlive();
     const dbStatus = dbClient.isAlive();
     response.set('Content-Type', 'application/json');
-    response.status(200).send(JSON.stringify({ redis: redisStatus, db: dbStatus })).end();
+    response.status(200);
+    response.json({ redis: redisStatus, db: dbStatus }).end();
+  }
+
+  static async getStats(request, response) {
+    const usersCount = await dbClient.nbUsers();
+    const filesCount = await dbClient.nbFiles();
+    response.set('Content-Type', 'application/json');
+    response.status(200);
+    response.json({ users: usersCount, files: filesCount }).end();
   }
 }
 
