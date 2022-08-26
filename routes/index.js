d6b1d156276f08c5f98aa00d9618fa7c94692d90
Pericles001 <periclesadjovi@gmail.com>
2022-08-26 22:53:09 +0100

    Update file

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,31 @@ import AppController from '../controllers/AppController';
 import UsersController from '../controllers/UsersController';
 import AuthController from '../controllers/AuthController';
 import FilesController from '../controllers/FilesController';
+import UtilController from '../controllers/UtilController';
 
 const router = Router();
+
+router.use((request, response, next) => {
+  const paths = ['/connect'];
+  if (!paths.includes(request.path)) {
+    next();
+  } else if (!request.headers.authorization) {
+    response.status(401).json({ error: 'Unauthorized' }).end();
+  } else {
+    next();
+  }
+});
+
+router.use((request, response, next) => {
+  const paths = ['/disconnect', '/users/me', '/files'];
+  if (!paths.includes(request.path)) {
+    next();
+  } else if (!request.headers['x-token']) {
+    response.status(401).json({ error: 'Unauthorized' }).end();
+  } else {
+    next();
+  }
+});
 
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
@@ -13,5 +36,9 @@ router.post('/users', UsersController.postNew);
 router.get('/connect', AuthController.getConnect);
 router.get('/disconnect', AuthController.getDisconnect);
 router.post('/files', FilesController.postUpload);
+router.get('/files/:id', FilesController.getShow);
+router.get('/files', FilesController.getIndex);
 
+router.put('/files/:id/publish', UtilController.token, FilesController.putPublish);
+router.put('/files/:id/unpublish', UtilController.token, FilesController.putUnpublish);
 module.exports = router;
