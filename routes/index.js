8320e5d7d4c4e94fb278bebf9dd662b412762d2f
Pericles001 <periclesadjovi@gmail.com>
2022-08-26 13:03:38 +0100

    Import files controller

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ import { Router } from 'express';
 import AppController from '../controllers/AppController';
 import UsersController from '../controllers/UsersController';
 import AuthController from '../controllers/AuthController';
+import FilesController from '../controllers/FilesController';
 
 const router = Router();
 
